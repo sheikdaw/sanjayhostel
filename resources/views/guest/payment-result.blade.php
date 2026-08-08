a2494965bebaee53a3c21eef23feb8af09d4cdb0
SGTSOLUTIONS <sgtsolutions24@gmail.com>
2026-08-08 13:42:52 +0530

    main

--- resources/views/guest/payment-result.blade.php
+++ resources/views/guest/payment-result.blade.php
@@ -8,7 +8,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css" rel="stylesheet">
-
+<!-- data -->
     <style>
         :root {
             --primary-color: #1a3a5c;
