3c242879d4f209eeb8881356f3b473344d1a7e5c
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2026-08-14 23:29:21 +0530

    main

--- resources/views/admin/hostels/index.blade.php
+++ resources/views/admin/hostels/index.blade.php
@@ -689,10 +689,15 @@
 
 <!-- Toast Container -->
 <div class="toast-container" id="flashMessageContainer"></div>
-{{--
+@endsection
+
+{{-- ============================================
+JAVASCRIPT - FIXED FILTERS WITH DEBUG
+============================================ --}}
+@push('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
 $(document).ready(function() {
